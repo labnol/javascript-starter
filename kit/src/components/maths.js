7ebd31fd5d2002fffa2025d60be7c4cd4de46159
Amit Agarwal <labnol@macbook.local>
2018-05-31 19:55:33 +0530

    Removed zip

--- kit/src/components/maths.js
+++
@@ -1,8 +0,0 @@
-const add = (a = 12, b = 13) => a + b;
-
-const subtract = (a, b) => {
-  if (a < b) return b - a;
-  return a - b;
-};
-
-export { add, subtract };
--- kit/src/index.js
+++
@@ -1,8 +0,0 @@
-import './styles/index.scss';
-import { add } from './components/maths';
-
-const x = 2;
-const y = 4;
-
-const root = document.getElementById('root');
-root.innerHTML = `${x} + ${y} = ${add(x, y)}`;
